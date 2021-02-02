c09d6bd422d0cea6e61dfe09e281fed7e57f074b
Jason Kmieciak <jason.kmieciak@mymail.champlain.edu>
2021-02-01 21:35:46 -0500

    Sprint one Prototype

---
+++ Sprint/Assets/Scrpts/Attacked.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacked : MonoBehaviour
+{
+    
+
+    private bool pickUpAllowed;
+
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.F))
+            PickUp();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        ScoreScript.scoreValue += 1;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Sprint/Assets/Scrpts/playerControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class playerControl : MonoBehaviour
+
+{
+    public float MovementSpeed = 1;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(moveHorizontal, 0, 0) * Time.deltaTime * MovementSpeed;
+        transform.position += new Vector3(0, moveVertical, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
+
